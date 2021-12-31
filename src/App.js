1eb2398f8b727f1bce394e9f598172c7a71f02e1
saryokodev <saryoko271292@gmail.com>
2021-12-31 22:36:13 +0700

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import './App.css'; 
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="e41e9f80-cb66-4346-a2dc-7c44d2ab21b3"
+            userName="JavascriptMastery" 
+            userSecret="123456"
+        
+        />
+    )
+}
+
+export default App; 
