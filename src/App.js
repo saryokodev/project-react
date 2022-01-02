4a5b88b0357e5a541f88ba531983aa9ae56b4e19
saryokodev <saryoko271292@gmail.com>
2022-01-02 22:06:04 +0700

    updated files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+
+
 import './App.css'; 
 
 const App = () => {
@@ -7,8 +10,9 @@ const App = () => {
         <ChatEngine 
             height="100vh"
             projectID="e41e9f80-cb66-4346-a2dc-7c44d2ab21b3"
-            userName="JavascriptMastery" 
+            userName="AdminGroup" 
             userSecret="123456"
+            renderChatFeed= {(chatAppProps) => <ChatFeed {...chatAppProps} /> }
         
         />
     )
